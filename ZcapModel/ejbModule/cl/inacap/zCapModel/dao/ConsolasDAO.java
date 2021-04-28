d1ee75270ef383de77d365f28b7bd6489cba241a
cururo <seba_18_2248@hotmail.com>
2021-04-27 23:34:49 -0500

    Generar base del proyecto erar

---
+++ ZcapModel/ejbModule/cl/inacap/zCapModel/dao/ConsolasDAO.java
@@ -0,0 +1,20 @@
+package cl.inacap.zCapModel.dao;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ConsolasDAO
+ */
+@Stateless
+@LocalBean
+public class ConsolasDAO implements ConsolasDAOLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public ConsolasDAO() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
