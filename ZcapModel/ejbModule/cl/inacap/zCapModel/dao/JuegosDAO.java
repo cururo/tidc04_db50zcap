22fa8c93f219992b1af6681501551e785bfef8c2
cururo <seba_18_2248@hotmail.com>
2021-04-28 00:09:07 -0500

    generar el dao de juegos

---
+++ ZcapModel/ejbModule/cl/inacap/zCapModel/dao/JuegosDAO.java
@@ -0,0 +1,20 @@
+package cl.inacap.zCapModel.dao;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class JuegosDAO
+ */
+@Stateless
+@LocalBean
+public class JuegosDAO implements JuegosDAOLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public JuegosDAO() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
